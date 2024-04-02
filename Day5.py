8d5d114af0ef202cdd1746f0834386cca844494d
Vik2110 <64458434+Vik2110@users.noreply.github.com>
2024-04-02 15:34:16 +0530

    python program for "while" and "for" loop

---
+++ Day5.py
@@ -0,0 +1,10 @@
+i=0
+names=["Alice", "Bob", "Charlie", "David", "Eve"]
+
+while ( i < 10 ):
+    i = i+1
+    print(i, end=' ')
+
+print('\n')
+for name in names:
+    print("Hello," +name+ "!", end=' ')
